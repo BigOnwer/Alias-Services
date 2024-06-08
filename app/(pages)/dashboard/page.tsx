20a4b0cbeb5b502db22307d5d300b553422b952c
BigOnwer <lealgustavo0099@gmail.com>
2024-06-08 11:16:14 -0300

    Erro no reduce

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -3,15 +3,18 @@
 import { Sidebar } from "@/components/SideBar";
 import { ContentDialog } from "@/components/Dashboard/ContentDialog";
 import { Summary } from "@/components/Dashboard/Summary";
+import { CardProvider } from "@/Contexts/ChartsContext";
 
 export default function Dashboard() {
     return (
         <div className="flex h-screen overflow-hidden">
             <Sidebar currentPage='dashboard' />
             <div className="flex-grow flex flex-col my-5 overflow-auto">
-                <div className="flex justify-start mx-2 md:mt-0">
-                    <ContentDialog />
-                </div>
+                <CardProvider>
+                    <div className="flex justify-start mx-2 md:mt-0">
+                        <ContentDialog />
+                    </div>
+                </CardProvider>
                 <Summary />
             </div>
         </div>
