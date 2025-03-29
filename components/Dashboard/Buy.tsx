ee6f11bb69371ed1c57ea054665d3664daa9c653
Gustavo leal <lealgustavo0099@gmail.com>
2025-03-29 18:00:16 -0300

    dashboard buy price component

--- components/Dashboard/Buy.tsx
+++ components/Dashboard/Buy.tsx
@@ -10,12 +10,14 @@ export function Buy() {
                 title="Business Dashboard"
                 description="Find out how your company's results are going"
                 imageSrc={DashboardExemple}
+                price="70,00"
                 buttonPush={process.env.NEXT_PUBLIC_STRIPE_PRICE_ID || ""}
                 />
                 <ServicesComponent
                 title="Creation of Websites"
                 description="Creating of Web Sites destined for business"
                 imageSrc={creattingWebSite}
+                price="30,00"
                 buttonPush="#"
                 />
             </main>
